ee2a6bbea5d58428e414b515a0e265a178e87f4c
test <andrey_sl_24@mail.ru>
2022-03-05 13:28:35 +0300

    Сделан общий файл банковского сектора Добавлена комиссия Теперь перевод возможен только через банк Теперь функция push_money public, но работает только если владелец -- ЦБ Добавлены getterы и сеттеры Добавлена процедура проверки ключа лицевого счёта Теперь все деньги -- отдельный класс, это ТЗ

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-from Personal_account import *
+from Banking_sector import *
 
-class Bank:
-    def __init__(self):
-        self.bank_account = PersonalAccount("lol", "Bank")
-
-
-class Money:
-    def __init__(self):
-        self.amount = 0
-
-
-CBank = Bank()
-print(CBank.bank_account.get_owner())
+CBank = Bank
 print(CBank.bank_account.get_balance())
-#print(CBank.bank_account.)
+a1 = PersonalAccount("AMOGUS", "Drew")
+a2 = PersonalAccount("AMOGUS", "Egor")
+#print(Bank.bank_account)
+CBank.transfer(CBank, CBank.bank_account, a2, "123", 500)
+print(a2.get_balance())
+print(CBank.bank_account.get_balance())
+CBank.transfer(CBank, a2, a1, "AMOGUS", 100)
+print(a2.get_balance())
+print(a1.get_balance())
+print(CBank.bank_account.get_balance())
